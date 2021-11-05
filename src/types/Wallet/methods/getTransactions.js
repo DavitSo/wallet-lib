ecc74358ee6f3b74cef8eb0848019edc65e74fb6
Davit <david.sokhakyan@gmail.com>
2021-11-05 06:22:50 -0700

    get address transactions

--- src/types/Wallet/methods/getTransactions.js
+++ src/types/Wallet/methods/getTransactions.js
@@ -2,6 +2,15 @@ module.exports = function getTransactions(address, offset = undefined, limit = u
   if (!address || address.length === 0) {
     throw new Error('Expect valid address');
   }
+  if (limit < 1 || !Number.isInteger(limit)) {
+    throw new Error('Expected valid limit value for request parameter');
+  }
+  if (offset < 0 || !Number.isInteger(limit)) {
+    throw new Error('Expected valid offset value for request parameter');
+  }
+  if (offset && !limit) {
+    throw new Error('Offset must be specified with limit parameter');
+  }
   const { transport } = this;
   return transport.getAddressTransactions(address, offset, limit);
 };
