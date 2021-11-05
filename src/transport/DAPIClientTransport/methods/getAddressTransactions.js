eedc29e1d7f72582b323191e1dd8e7f435d46e90
Davit <david.sokhakyan@gmail.com>
2021-11-05 05:54:27 -0700

    get address transactions

--- src/transport/DAPIClientTransport/methods/getAddressTransactions.js
+++ src/transport/DAPIClientTransport/methods/getAddressTransactions.js
@@ -1,6 +1,6 @@
 const logger = require('../../../logger');
 
-module.exports = async function getAddressTransactions(address) {
+module.exports = async function getAddressTransactions(address, offset, limit) {
   logger.silly('DAPIClientTransport.getAddressTransactions');
-  return this.client.core.getAddressTransactions(address);
+  return this.client.core.getAddressTransactions(address, offset, limit);
 };
--- src/types/Wallet/methods/getTransactions.js
+++ src/types/Wallet/methods/getTransactions.js
@@ -1,7 +1,7 @@
-module.exports = function getTransactions(address) {
+module.exports = function getTransactions(address, offset = undefined, limit = undefined) {
   if (!address || address.length === 0) {
     throw new Error('Expect valid address');
   }
   const { transport } = this;
-  return transport.getAddressTransactions(address);
+  return transport.getAddressTransactions(address, offset, limit);
 };
