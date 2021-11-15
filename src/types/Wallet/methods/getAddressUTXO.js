3fa4b70290a4c06a1251c669b4a81f238a7d8cbd
Davit <david.sokhakyan@gmail.com>
2021-11-15 09:49:30 -0800

    get address utxo  commit

--- src/types/Wallet/methods/getAddressUTXO.js
+++ src/types/Wallet/methods/getAddressUTXO.js
@@ -1,7 +1,23 @@
+const { Transaction, Address } = require('@dashevo/dashcore-lib');
+
 module.exports = function getAddressUTXO(address) {
   if (!address || address.length === 0) {
     throw new Error('Expect valid address');
   }
   const { transport } = this;
-  return transport.getAddressUTXO(address);
+  const result = transport.getAddressUTXO(address);
+  const utxos = [];
+
+  result.utxos.forEach((utxo) => {
+    utxos.push(new Transaction.UnspentOutput(
+      {
+        txId: utxo.transactionId,
+        vout: parseInt(utxo.outputIndex, 10),
+        script: utxo.script,
+        satoshis: utxo.satoshis,
+        address: new Address(utxo.address, this.network),
+      },
+    ));
+  });
+  return utxos.sort((a, b) => b.satoshis - a.satoshis);
 };
