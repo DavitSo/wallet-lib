48b11fee266b71bb5bf3a7ef86b9e971712000d3
Davit <david.sokhakyan@gmail.com>
2021-11-15 09:57:29 -0800

    get address utxo  commit

--- src/types/Wallet/methods/getAddressUTXO.js
+++ src/types/Wallet/methods/getAddressUTXO.js
@@ -5,19 +5,22 @@ module.exports = function getAddressUTXO(address) {
     throw new Error('Expect valid address');
   }
   const { transport } = this;
-  const result = transport.getAddressUTXO(address);
-  const utxos = [];
+  return transport.getAddressUTXO(address).then((result) => {
+    const utxos = [];
 
-  result.utxos.forEach((utxo) => {
-    utxos.push(new Transaction.UnspentOutput(
-      {
-        txId: utxo.transactionId,
-        vout: parseInt(utxo.outputIndex, 10),
-        script: utxo.script,
-        satoshis: utxo.satoshis,
-        address: new Address(utxo.address, this.network),
-      },
-    ));
+    result.utxos.forEach((utxo) => {
+      utxos.push(new Transaction.UnspentOutput(
+        {
+          txId: utxo.transactionId,
+          vout: parseInt(utxo.outputIndex, 10),
+          script: utxo.script,
+          satoshis: utxo.satoshis,
+          address: new Address(utxo.address, this.network),
+        },
+      ));
+    });
+    return utxos.sort((a, b) => b.satoshis - a.satoshis);
+  }).catch((error) => {
+    throw error;
   });
-  return utxos.sort((a, b) => b.satoshis - a.satoshis);
 };
